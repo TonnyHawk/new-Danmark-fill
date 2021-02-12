54a6e90f831f433f275408b4efb5be388eab21ae
TonnyHawk <anton.veremko@gmail.com>
2021-02-12 16:36:55 +0200

    head-instr slider finished

--- #src/styles/modules/head-instr/head-instr.js
+++ #src/styles/modules/head-instr/head-instr.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
+
+  // slider options
    $('.head-instr .slider__imgs').slick({
     adaptiveHeight: true,
     arrows: false,
+    asNavFor: '.head-instr .slider__txts .slider__wrap',
     responsive: [
       {
         breakpoint: 850,
@@ -10,5 +13,19 @@ $(document).ready(function(){
         }
       }
     ]
+   });
+
+   $('.head-instr .slider__txts .slider__wrap').slick({
+     arrows: false,
+     adaptiveHeight: true,
+     asNavFor: '.head-instr .slider__imgs',
+     fade: true
+   });
+
+   // tabs
+   $('.head-instr__tag').click(function(){
+    $(this).addClass('is-active').siblings().removeClass('is-active');
+    let tabName = $(this).html().toLowerCase();
+    $(".head-instr__tab[data-name='"+tabName+"']").addClass('is-active').siblings().removeClass('is-active');
    });
  });
